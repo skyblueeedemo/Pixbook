0eb82ca0432bf07097239bd41c1bd8fc09c7a3fc
Skyblueeeee <978744267@qq.com>
2026-05-24 19:37:38 +0800

    fix: import AuthModule in ConfigModule so AuthGuard can resolve JwtService

--- packages/server/src/modules/config/config.module.ts
+++ packages/server/src/modules/config/config.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigController } from './config.controller';
 import { ConfigService } from './config.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [ConfigController],
   providers: [ConfigService],
   exports: [ConfigService],
